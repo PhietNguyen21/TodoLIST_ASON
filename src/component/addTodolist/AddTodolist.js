5d9d526c68d79decbad88ecad28b607e4eb350b8
Nphiet <phietnguyen321@gmail.com>
2023-10-08 20:26:28 +0700

    upload

---
+++ src/component/addTodolist/AddTodolist.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+
+const AddTodolist = ({ addTodo, arrTodo, completedTodo }) => {
+  const [content, setContent] = useState("");
+
+  const handleClickAdd = (e) => {
+    // console.log(content);
+    e.preventDefault();
+    const newTodo = {
+      id: Math.floor(Math.random() * 10000 + 1),
+      content: content,
+      isCompleted: false,
+    };
+
+    addTodo(newTodo);
+    setContent("");
+    // console.log({ arrTodo });
+  };
+
+  const onChange = (e) => {
+    const { checked, value } = e.target;
+
+    completedTodo(checked, value);
+  };
+  return (
+    <div>
+      <Form
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Form.Item style={{ width: 500 }}>
+          <Input
+            name="content"
+            value={content}
+            placeholder="add details"
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+          />
+        </Form.Item>
+        <Button
+          type="primary"
+          onClick={handleClickAdd}
+          //   htmlType="submit"
+        >
+          Add
+        </Button>
+      </Form>
+
+      {arrTodo?.map((item, index) => {
+        console.log("ITEM", item);
+        console.log({ arrTodo });
+        return (
+          <div key={index}>
+            <Checkbox
+              onChange={onChange}
+              value={item.id}
+              checked={item.isCompleted}
+            >
+              {item.isCompleted ? (
+                <b style={{ textDecorationLine: "line-through" }}>
+                  {item.content}
+                </b>
+              ) : (
+                <b>{item.content}</b>
+              )}
+            </Checkbox>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default AddTodolist;
---
+++ src/component/deleteTodolist/DeleteTodolist.js
@@ -0,0 +1,54 @@
+import { Button, Checkbox } from "antd";
+import { useEffect, useState } from "react";
+import React from "react";
+import { DeleteOutlined } from "@ant-design/icons";
+
+const DeleteTodolist = ({ arrTodo, completedTodo, deleteTodo, deleteAll }) => {
+  const onChange = (e) => {
+    const { checked, value } = e.target;
+    completedTodo(checked, value);
+  };
+
+  return (
+    <div>
+      <ul>
+        {arrTodo
+          ?.filter((todo) => todo.isCompleted)
+          .map((item, index) => {
+            console.log("okok", item);
+            return (
+              <div>
+                <Checkbox
+                  onChange={onChange}
+                  value={item.id}
+                  checked={item.isCompleted}
+                >
+                  {item.isCompleted ? (
+                    <b style={{ textDecorationLine: "line-through" }}>
+                      {item.content}
+                    </b>
+                  ) : (
+                    <b>{item.content}</b>
+                  )}
+                </Checkbox>
+                <DeleteOutlined
+                  onClick={() => {
+                    deleteTodo(item.id);
+                  }}
+                />
+              </div>
+            );
+          })}
+      </ul>
+      <Button
+        onClick={() => {
+          deleteAll();
+        }}
+      >
+        Clear All
+      </Button>
+    </div>
+  );
+};
+
+export default DeleteTodolist;
